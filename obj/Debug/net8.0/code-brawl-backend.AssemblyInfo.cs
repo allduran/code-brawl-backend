4e5b207e3c99b03787a56b3d9e7a49733e0b4911
duran <cheshire.dd96@gmail.com>
2024-08-05 10:28:50 +0200

    update backend.yml

--- obj/Debug/net8.0/code-brawl-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/code-brawl-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("code-brawl-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d4759d491b37a6af5527aca7a5f882e115d20dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a319342d7048e41e40a546da05ef34dd89ef30")]
 [assembly: System.Reflection.AssemblyProductAttribute("code-brawl-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("code-brawl-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
