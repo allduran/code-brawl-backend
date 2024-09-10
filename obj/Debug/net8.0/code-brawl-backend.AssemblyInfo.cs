a9f59d82fb91a52f9b963e58aa7d7ae5a1b12fa7
duran <cheshire.dd96@gmail.com>
2024-09-10 16:47:20 +0200

    added something to the backend.AssemblyInfo.cs

--- obj/Debug/net8.0/code-brawl-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/code-brawl-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("code-brawl-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a319342d7048e41e40a546da05ef34dd89ef30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e914b037c4dc9c581975b9f0319851965e8d12d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("code-brawl-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("code-brawl-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
